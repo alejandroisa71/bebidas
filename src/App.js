745818015fd46e93d5205f6ecfccd3e8146a8d6c
Alejandro Isa <alejandroisa@gmail.com>
2021-02-15 19:58:32 -0300

    se agrega componente Formulario.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <div className="container mt-5">
+        <div className="row">
+          <Formulario />
+        </div>
+      </div>
     </>
   );
 }
